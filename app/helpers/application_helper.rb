ef7aecd8562c32bf94ba9cec4e4579c107def4e1
Michał Narajowski <michal.narajowski@gmail.com>
2014-07-24 17:35:24 +0200

    Styled flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def flash_class(type)
+		
+	    case type.to_s
+		    when "alert"
+		    	"alert-warning"
+		    when "notice" 
+		    	"alert-info"
+		    when "success" 
+		    	"alert-success"
+		    when "error" 
+		    	"alert-danger"
+		    else
+		    	type.to_s
+	   	end
+    end
 end
